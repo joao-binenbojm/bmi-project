db16acbd260a60d815bd2fc0ef6a8549dbdd2889
Rubén Ruiz-Mateos Serrano <rr1017@ic.ac.uk>
2021-02-22 16:18:57 +0000

    First attempt of tuning_curve function. Attempted two possible firing rates: rate as spike count averaged over trials and rate as spike density averaged over time and trials.

--- tuning_curve.m
+++ tuning_curve.m
@@ -1,22 +1,44 @@
-function [] = tuning_curve(data)
-    % [] = TUNING_CURVE(data)
+function [] = tuning_curve(data,unit,dt)
+    % [] = TUNING_CURVE(data,unit,dt)
     % data - given struct array 
-    % sel - struct with fields:
-        %.trial - range or single trial value
-        %.angle - range or single angle value
-        %.unit - range or single unit value
-    % opt - string argument: 
-        % 'trial' - displays trials in array sel 
-        % for same unit and angle
-        % 'unit' - displays units in array sel
-        % for same trial and angle
-        % 'angle' - displays angles in array sel
-        % for same unit and trial  
-    % type - string argument: 
-        % 'dot' - displays colour scatter plot
-        % 'trial' - displays monocromatic line plot
-        
-        
+    % unit - scalar determining unit
+    % dt - scalar determining time interval in ms
+    
+    [T,A] = size(data);
+    angle_list = [30/180*pi,70/180*pi,110/180*pi,150/180*pi,190/180*pi,230/180*pi,310/180*pi,350/180*pi];
+   
+    % Rate as spike count
+    
+%     for j = 1:1:A
+%         for i = 1:1:T
+%             n = length(data(i,j).spikes(unit,:));
+%             counts = sum(data(i,j).spikes(unit,:));
+%             rate = counts/n;
+%             total(i) = mean(rate);
+%         end
+%         fr(j) = mean(total);
+%         fr_std(j) = std(total);
+%     end
+    
+    % Rate as spike density
+    
+    for j = 1:1:A
+        for i = 1:1:T
+            n = length(data(i,j).spikes(unit,:));
+            counts = histcounts([1:1:n].*data(i,j).spikes(unit,:),0:dt:n);
+              rate = counts/(T*dt);
+            time_avg(i) = mean(rate);
+        end
+        fr(j) = mean(time_avg);
+        fr_std(j) = std(time_avg);
+    end
+    
+    figure;
+    errorbar(angle_list,fr,fr_std,'-s','LineWidth',2,'Color','k','MarkerSize',10,...
+    'MarkerEdgeColor','red','MarkerFaceColor','red');
+    set(gca,'FontSize',15);
+    xlabel('Angle [rad]','FontSize',20);
+    ylabel('Firing rate [spikes/(s$\times$trial)]','FontSize',20);
 
     
 end
