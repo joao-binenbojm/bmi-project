f64c4abdae02684dbdc78cf166b1918830745853
Rubén Ruiz-Mateos Serrano <rr1017@ic.ac.uk>
2021-02-22 12:31:37 +0000

    Created tuning_curve function file

---
+++ tuning_curve.m
@@ -0,0 +1,23 @@
+function [] = tuning_curve(data)
+    % [] = TUNING_CURVE(data)
+    % data - given struct array 
+    % sel - struct with fields:
+        %.trial - range or single trial value
+        %.angle - range or single angle value
+        %.unit - range or single unit value
+    % opt - string argument: 
+        % 'trial' - displays trials in array sel 
+        % for same unit and angle
+        % 'unit' - displays units in array sel
+        % for same trial and angle
+        % 'angle' - displays angles in array sel
+        % for same unit and trial  
+    % type - string argument: 
+        % 'dot' - displays colour scatter plot
+        % 'trial' - displays monocromatic line plot
+        
+        
+
+    
+end
+
