05a499ac56342f3d6222c2dd887156f43e0ce4a5
Rubén Ruiz-Mateos Serrano <rr1017@ic.ac.uk>
2021-02-23 08:17:39 +0000

    Updated help text

--- position_plot.m
+++ position_plot.m
@@ -13,6 +13,7 @@ function [] = position_plot(data,sel,type)
             plot_data = data(i,j).handPos;
             
             if strcmpi(type,'2d')
+                figure;
                 hold on;
                 plot(plot_data(1,:),plot_data(2,:),'LineWidth',2);
                 set(gca,'FontSize',15);
@@ -20,6 +21,7 @@ function [] = position_plot(data,sel,type)
                 xlabel('$x$ [mm]','FontSize',20);
                 ylabel('$y$ [mm]','FontSize',20); 
             elseif strcmpi(type,'3d')
+                figure;
                 hold on;
                 plot3(plot_data(1,:),plot_data(2,:),plot_data(3,:),'LineWidth',2);
                 set(gca,'FontSize',15);
