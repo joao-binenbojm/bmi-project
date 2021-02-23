a64cae5b7e496fa53d7a9dba7f73ec604355c8f1
Rubén Ruiz-Mateos Serrano <rr1017@ic.ac.uk>
2021-02-23 10:50:59 +0000

    Included detrending option

--- F_analysis.m
+++ F_analysis.m
@@ -1,19 +1,26 @@
-function F_data = F_analysis(trial)
+function F_data = F_analysis(trial,opt)
     % [] = F_ANALYSIS(trial)
         % trial - given struct array 
+        % opt - string argument:
+            % detrend - fft of detrended data
+            % [] - fft of raw data
         % F_data - Fourier transform of data
         
-data = extractfield(trial,'spikes');
+    data = extractfield(trial,'spikes');
 
-[T,A,U,~] = size(data);
+    [T,A,U,~] = size(data);
 
-F_data = NaN(T,A,U,1000);
-for i = 1:1:T
-    for j = 1:1:A
-        for k = 1:1:U
-            var = data(i,j,k,:);
-            nonan_len = length(var(~isnan(var)));
-            F_data(i,j,k,1:nonan_len) = abs(fftshift(fft(data(i,j,k,1:1:nonan_len))));
+    F_data = NaN(T,A,U,1000);
+    for i = 1:1:T
+        for j = 1:1:A
+            for k = 1:1:U
+                var = data(i,j,k,:);
+                nonan_len = length(var(~isnan(var)));
+                if strcmp(opt,'detrend')
+                    x = detrend(squeeze(data(i,j,k,1:1:nonan_len)));
+                end
+                F_data(i,j,k,1:nonan_len) = abs(fftshift(fft(squeeze(x))));
+            end
         end
     end
 end
