c6a22f33ec8179f6ab300b4e467454977edab1b4
Rubén Ruiz-Mateos Serrano <rr1017@ic.ac.uk>
2021-02-23 08:02:33 +0000

    Modified the function to allow for different ranges of angles and trials

--- position_plot.m
+++ position_plot.m
@@ -2,33 +2,36 @@ function [] = position_plot(data,sel,type)
     % [] = POSITION_PLOT(data,sel,type)
     % data - given struct array 
     % sel - struct with fields:
-        %.trial - single trial value
-        %.angle - single angle value    
+        %.trial - range or single trial value
+        %.angle - range or single angle value    
     % type - string argument: 
         % '2d' - displays X,Y dimensions
         % '3d' - displays X,Y,Z dimensions
-
-    if length(sel.trial) ~=1 || length(sel.angle) ~=1
-        error('Invalid selection: *sel* must contain a single trial and angle');
+    
+    for i = 1:1:length(sel.trial)
+        for j = 1:1:length(sel.angle)
+            plot_data = data(i,j).handPos;
+            
+            if strcmpi(type,'2d')
+                hold on;
+                plot(plot_data(1,:),plot_data(2,:),'LineWidth',2);
+                set(gca,'FontSize',15);
+                grid on;
+                xlabel('$x$ [mm]','FontSize',20);
+                ylabel('$y$ [mm]','FontSize',20); 
+            elseif strcmpi(type,'3d')
+                hold on;
+                plot3(plot_data(1,:),plot_data(2,:),plot_data(3,:),'LineWidth',2);
+                set(gca,'FontSize',15);
+                grid on;
+                xlabel('$x$ [mm]','FontSize',20);
+                ylabel('$y$ [mm]','FontSize',20); 
+                zlabel('$z$ [mm]','FontSize',20); 
+            else
+                error('Invalid selection: type must be *2d* or *3d*');
+            end
+        end
     end
-    plot_data = data(sel.trial,sel.angle).handPos;
-
-    if strcmpi(type,'2d')
-        plot(plot_data(1,:),plot_data(2,:),'LineWidth',2,'Color','b');
-        set(gca,'FontSize',15);
-        xlabel('$x$ [mm]','FontSize',20);
-        ylabel('$y$ [mm]','FontSize',20); 
-    elseif strcmpi(type,'3d')
-        plot3(plot_data(1,:),plot_data(2,:),plot_data(3,:),'LineWidth',2,'Color','b');
-        set(gca,'FontSize',15);
-        grid on;
-        xlabel('$x$ [mm]','FontSize',20);
-        ylabel('$y$ [mm]','FontSize',20); 
-        zlabel('$z$ [mm]','FontSize',20); 
-    else
-        error('Invalid selection: type must be *2d* or *3d*');
-    end
-
     
 end
 
