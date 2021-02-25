0a2d11c14979b613ed6670b5f74ad7c3101b96fe
Julia Gimbernat <jg917@imperial.ac.uk>
2021-02-25 17:54:44 +0000

    PCA code to visualize classes

---
+++ PCA.m
@@ -0,0 +1,26 @@
+%% PCA
+
+load features.mat
+load labels_tuning_curve.mat
+
+s = sum(peaks/1);
+peaks_norm = peaks./s;
+[coeff,score,latent,tsquared,explained,mu] = pca(peaks_norm','NumComponents',3);
+figure;
+hold on;
+c=labels; % colors labels
+scatter3(score(:,1),score(:,2),score(:,3),[],c,'filled')
+axis equal
+xlabel('1st Principal Component')
+ylabel('2nd Principal Component')
+zlabel('3rd Principal Component')
+l = 1:98; % units labels
+text( score(:,1),score(:,2), score(:,3),cellstr(num2str(l')) )
+
+figure;
+pareto(explained)
+
+figure;
+plot(explained)
+
+explained(1)+explained(2) %total explained variance
