a9fe84f870ed9a3e6df5946e7cdf3284c26189b6
Julia Gimbernat <jg917@imperial.ac.uk>
2021-02-27 15:20:22 +0000

    clustering per units to get p-vect

--- hierarchical_clustering.m
+++ hierarchical_clustering.m
@@ -1,5 +1,25 @@
 %% Hierarchical clustering
 
+%% DATA
+% 1 angle
+data=extractfield(trial(:,1),'spikes');
+data=squeeze(mean(data,3));
+data=data(:,1:593);
+
+% all angles
+data=extractfield(trial,'spikes');
+data=squeeze(mean(data,3));
+data=data(:,1:593,:);
+data=reshape(data,784,593);
+
+% 1 unit, all angles
+data=extractfield(trial,'spikes');
+data=squeeze(mean(data,3));
+data=squeeze(data(20,1:571,:));
+data=data';
+
+%%
+
 load features.mat
 load labels_tuning_curve.mat
 
@@ -8,16 +28,16 @@ peaks_norm = peaks./s;
 [coeff,score,latent,tsquared,explained,mu] = pca(peaks_norm','NumComponents',3);
 
 %X=score;
-X=peaks';
+X=data;
 
-Y = pdist(X);
+Y = pdist(X,'correlation');
 Y_square = squareform(Y);
 
 Z = linkage(Y);
-dendrogram(Z,98)
+dendrogram(Z)
 
-T = cluster(Z,'maxclust',8);
-
+T = cluster(Z,'maxclust',3);
+[coeff,score,latent,tsquared,explained,mu] = pca(data,'NumComponents',3);
 figure;
 hold on;
 scatter3(score(:,1),score(:,2),score(:,3),[],T,'filled')
@@ -29,6 +49,8 @@ l = 1:98; % units labels
 text( score(:,1),score(:,2), score(:,3),cellstr(num2str(l')) )
 
 %% correlation distance
+
 Y = pdist(X,'correlation');
 Z = linkage(Y,'average');
 c = cophenet(Z,Y)
+
