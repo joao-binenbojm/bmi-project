d8e2f37921b2a73bf02f31b69848de366b02a909
Rubén Ruiz-Mateos Serrano <rr1017@ic.ac.uk>
2021-03-06 09:57:32 +0000

    Edited tuning_curve() to include continuous angle estimation. Created population vector encoder (not in required format since it is very bad. RMSE=800!).

--- funcs/tuning_curve.m
+++ funcs/tuning_curve.m
@@ -1,4 +1,4 @@
- function peak = tuning_curve(data,unit,dt,opt,show)
+function peak = tuning_curve(data,unit,dt,opt,show)
     % peak = TUNING_CURVE(data,unit,dt,opt,show)
     % data - given struct array 
     % unit - scalar determining unit
@@ -9,8 +9,12 @@
     % show - string argument: 
         % 'show' - plots graph
     % peak - struct with fields
-        % .idx - indicates the preferred angle
-        % .values - stores the average firing rate over angles
+        % .idx - stores the preferred angle
+        % .mean_values - stores the average firing rate over angles
+        % .std_values - stores the standard deviation of the firing rate
+        % over angles
+        % .Fit - stores the exponential cosine function fitted to the data
+        
         
     [T,A] = size(data);
     angle_list = [30/180*pi,70/180*pi,110/180*pi,150/180*pi,190/180*pi,230/180*pi,310/180*pi,350/180*pi];
@@ -49,7 +53,7 @@
                 var = data(i,j).spikes(unit,:);
                 var(var==0) = NaN;
                 counts = histcounts([1:1:n].*var,0:dt:n);
-                  rate = counts/(dt);
+                rate = counts/(dt);
                 time_avg(i) = mean(rate);
             end
             fr(j) = mean(time_avg);
@@ -77,7 +81,11 @@
     
     [~,peak.idx] = max(fr);
     
+    fun = @(p,theta) exp(p(1)+p(2)*cos(theta-p(3)));
+    Fit = nlinfit(angle_list,fr,fun,[0,0,0]);
     if strcmpi(show,'show')
+        plot(linspace(0,2*pi,100),fun(Fit,linspace(0,2*pi,100)),'Color','b','LineWidth',2);
+        hold on;
         errorbar(angle_list,fr,fr_std,'-s','LineWidth',2,'Color','k','MarkerSize',10,...
         'MarkerEdgeColor','red','MarkerFaceColor','red');
         set(gca,'FontSize',15);
@@ -85,6 +93,8 @@
         ylabel('Firing rate [spikes/s]','FontSize',20);
     end
     
-    peak.values = fr;
+    peak.mean_values = fr;
+    peak.std_values = fr_std;
+    peak.Fit = fun(Fit,linspace(0,2*pi,100));
 end
 
