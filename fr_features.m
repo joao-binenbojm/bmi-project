dc130c9de9cb383387f5717b8b5b746ca4e7f431
Rubén Ruiz-Mateos Serrano <rr1017@ic.ac.uk>
2021-03-06 11:16:31 +0000

    Added more comments.

--- fr_features.m
+++ fr_features.m
@@ -9,20 +9,20 @@ function [fr_total, fr_avg] = fr_features(data,dt,N)
     [T,A] = size(data); %get trial and angle length
     
     acc = 1;
-    fr_avg = zeros(T*A,98);
+    fr_avg = zeros(T*A,98); % initialise variables
     fr_total = zeros(T*A,N/dt*98);
     for t=1:1:T
         for a=1:1:A
             fr = zeros(98,length(0:dt:N)-1);
             for u=1:1:98
                 var = data(t,a).spikes(u,1:N);
-                var(var==0) = NaN;
-                count = histcounts([1:1:N].*var,0:dt:N);
+                var(var==0) = NaN; % make zeros equal to NaN
+                count = histcounts([1:1:N].*var,0:dt:N); % count spikes in every dt bin until N
                 fr(u,:) = count/dt;
             end
-            fr_avg(acc,:) = mean(fr,2);
+            fr_avg(acc,:) = mean(fr,2); % get mean firing rate across bins
             f = reshape(fr,size(fr,1)*size(fr,2),1);
-            fr_total(acc,:) = f;
+            fr_total(acc,:) = f; % get all firing rates ordered in 98 blocks of the same bin
             acc = acc+1;
         end
     end
