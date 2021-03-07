90847b91df084954d0ea3380514da83661404dc1
Rubén Ruiz-Mateos Serrano <rr1017@ic.ac.uk>
2021-03-07 14:56:51 +0000

    Created new training and testing files for MAP/Bayesian filter.

---
+++ positionEstimator.m
@@ -0,0 +1,17 @@
+function [x, y, newModelParameters] = positionEstimator(testData, modelParameters)
+    % - test_data:
+    %     test_data(m).trialID
+    %         unique trial ID
+    %     test_data(m).startHandPos
+    %         2x1 vector giving the [x y] position of the hand at the start
+    %         of the trial
+    %     test_data(m).decodedHandPos
+    %         [2xN] vector giving the hand position estimated by your
+    %         algorithm during the previous iterations. In this case, N is
+    %         the number of times your function has been called previously on
+    %         the same data sequence.
+    %     test_data(m).spikes(i,t) (m = trial id, i = neuron id, t = time)
+    %     in this case, t goes from 1 to the current time in steps of 20
+
+    
+end
