2a1c5a06e10045919a33334c882769000c822c8a
Rubén Ruiz-Mateos Serrano <rr1017@ic.ac.uk>
2021-03-09 11:57:51 +0000

    Left previous modification commented out.

--- positionEstimator_braniacs.m
+++ positionEstimator_braniacs.m
@@ -70,8 +70,8 @@ function [x, y, newModelParameters] = positionEstimator_braniacs(testData, model
 %     x = (x+x_prime+x_prime_2)/3;
 %     y = (y+y_prime+y_prime_2)/3;
 
-      x = (x+x_prime)/2;
-      y = (y+y_prime)/2;
+%     x = (x+x_prime)/2;
+%     y = (y+y_prime)/2;
 
 %     x = x_prime;
 %     y = y_prime;
