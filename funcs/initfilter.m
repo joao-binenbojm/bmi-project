1aeaf16b97e55f1464fa0c794acec5497776f487
Rubén Ruiz-Mateos Serrano <rr1017@ic.ac.uk>
2021-03-11 08:52:27 +0000

    Completed hanning window compressing function. It now allows also for any possible built-in Matlab window (e.g. hanning, hamming, rectwin, etc.).

--- funcs/initfilter.m
+++ funcs/initfilter.m
@@ -1,30 +1,34 @@
-function data_out = initfilter(data,J,overlap)
+function data_out = initfilter(data,J,overlap,window)
     % [] = INITFILTER(data,range)
     % data - given struct array 
     % J - number of Hanning windows (new number of columns)
     % overlap - percentage of overlap between windows (decimals)
     % data_out - filtered output in the same format as data 
+    % window - type of window specified as a string
+    
+    [T,A] = size(data); % get data size
 
     data_cell = struct2cell(data); % convert to cell matrix
     max_length = @(x) length(x); % function handle to get max length of all elements in cell
     l = cellfun(max_length,data_cell);
-    N = max(squeeze(l(3,:,:)),[],'all');
+    N = max(squeeze(l(3,:,:)),[],'all'); % get maximum length
    
-    T = floor(N/((1-overlap)*J));
+    L = round(N/(0.5+(1-overlap)*(J-1))); % determine window size
     
     H = zeros(J,N);
-    for j = 1:J
-        shift = floor((j-1)*(1-overlap)*T);
-        H(j,1:T+shift) = [zeros(1,shift) hanning(T)'];
+    for j = 1:J % create matrix with shifted versions of given window defined by overlap
+        shift = round((j-1)*(1-overlap)*L);
+        H(j,1:L+shift) = [zeros(1,shift) eval(sprintf('%s(L)',window))'];
     end
     
-    H(:,floor(1:T/2)) = [];
+    H(:,1:round(L/2)) = []; % neglect first L/2 values
     
-    for a = 1:A
+    for a = 1:A % apply kernel to spikes data for smoothing 
         for t = 1:T
             for u = 1:98
-                var = data(t,a).spikes(u,:);
-                
+                var = data(t,a).spikes(u,:)';
+                new_var = H*[var;zeros(length(H)-length(var),1)];
+                data_out(t,a).spikes(u,:) = new_var';
             end
         end
     end
