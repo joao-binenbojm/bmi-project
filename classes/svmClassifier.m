5d210a3de5a151fa1f84d8b5937acdeedbcd78d4
Rubén Ruiz-Mateos Serrano <rr1017@ic.ac.uk>
2021-03-14 08:53:03 +0000

    2 layers SVM, 1 layer LDA with optimal C and s. RMSE = 29.6432 and percentage = 67.44%.

--- classes/svmClassifier.m
+++ classes/svmClassifier.m
@@ -338,41 +338,41 @@ classdef svmClassifier < handle
             l3_high_low = l2_high(l2_high_log); 
             l3_high_low_log = l2_high(l3_high_low)<2;
             idx = labels<3;
-            model3_high_low = obj.svmTrain(fr_avg(idx,:), double(l3_high_low_log), C, @(x1, x2) obj.gaussianKernel(x1, x2, s));
-%             LDA_1_2 = fitcknn(fr_avg(idx,:),double(l3_high_low_log));
+%             model3_high_low = obj.svmTrain(fr_avg(idx,:), double(l3_high_low_log), C, @(x1, x2) obj.gaussianKernel(x1, x2, s));
+            LDA_1_2 = fitcdiscr(fr_avg(idx,:),double(l3_high_low_log));
 
             % 7 (1) - 8 (0)
             l3_high_high = l2_high(~l2_high_log); 
             l3_high_high_log = l2_high(l3_high_high)<8;
             idx = labels>6;
-            model3_high_high = obj.svmTrain(fr_avg(idx,:), double(l3_high_high_log), C, @(x1, x2) obj.gaussianKernel(x1, x2, s));
-%             LDA_7_8 = fitcknn(fr_avg(idx,:),double(l3_high_high_log));
+%             model3_high_high = obj.svmTrain(fr_avg(idx,:), double(l3_high_high_log), C, @(x1, x2) obj.gaussianKernel(x1, x2, s));
+            LDA_7_8 = fitcdiscr(fr_avg(idx,:),double(l3_high_high_log));
 
             % 3 (1) - 4 (0)
             l3_low_low = l2_low(l2_low_log);
             l3_low_low_log = l3_low_low<4;
             idx = and((labels>2),(labels<5));
-            model3_low_low = obj.svmTrain(fr_avg(idx,:), double(l3_low_low_log), C, @(x1, x2) obj.gaussianKernel(x1, x2, s));
-%             LDA_3_4 = fitcknn(fr_avg(idx,:),double(l3_low_low_log));
+%             model3_low_low = obj.svmTrain(fr_avg(idx,:), double(l3_low_low_log), C, @(x1, x2) obj.gaussianKernel(x1, x2, s));
+            LDA_3_4 = fitcdiscr(fr_avg(idx,:),double(l3_low_low_log));
 
             % 5 (1) - 6 (0)
             l3_low_high = l2_low(~l2_low_log); 
             l3_low_high_log = l3_low_high<6;
             idx = and((labels>4),(labels<7));
-            model3_low_high = obj.svmTrain(fr_avg(idx,:), double(l3_low_high_log), C, @(x1, x2) obj.gaussianKernel(x1, x2, s));
-%             LDA_5_6 = fitcknn(fr_avg(idx,:),double(l3_low_high_log));
+%             model3_low_high = obj.svmTrain(fr_avg(idx,:), double(l3_low_high_log), C, @(x1, x2) obj.gaussianKernel(x1, x2, s));
+            LDA_5_6 = fitcdiscr(fr_avg(idx,:),double(l3_low_high_log));
 
             predict.model1_3456_1278 = model1;
             predict.model2_34_56 = model2_low;
             predict.model2_12_78 = model2_high;
-            predict.model3_1_2 = model3_high_low;
-%             predict.LDA_1_2 = LDA_1_2;
-            predict.model3_7_8 = model3_high_high;
-%             predict.LDA_7_8 = LDA_7_8;
-            predict.model3_3_4 = model3_low_low;
-%             predict.LDA_3_4 = LDA_3_4;
-            predict.model3_5_6 = model3_low_high;
-%             predict.LDA_5_6 = LDA_5_6;
+%             predict.model3_1_2 = model3_high_low;
+            predict.LDA_1_2 = LDA_1_2;
+%             predict.model3_7_8 = model3_high_high;
+            predict.LDA_7_8 = LDA_7_8;
+%             predict.model3_3_4 = model3_low_low;
+            predict.LDA_3_4 = LDA_3_4;
+%             predict.model3_5_6 = model3_low_high;
+            predict.LDA_5_6 = LDA_5_6;
             
             out = predict;
             obj.model = out;
@@ -389,16 +389,16 @@ classdef svmClassifier < handle
             if pred_1 == 1 % left(1): 1, 2, 7, 8
                 pred_2 = obj.svmPredict(obj.model.model2_12_78, fr_avg(1,:)); % 1, 2 (1) - 7, 8 (0)
                 if pred_2 == 1 % 1 (1) - 2 (0)
-                    pred_3 = obj.svmPredict(obj.model.model3_1_2, fr_avg(1,:));
-%                     pred_3 = predict(obj.model.LDA_1_2, fr_avg(1,:));
+%                     pred_3 = obj.svmPredict(obj.model.model3_1_2, fr_avg(1,:));
+                    pred_3 = predict(obj.model.LDA_1_2, fr_avg(1,:));
                     if pred_3 == 1
                         obj.pred_angle = 1;
                     else 
                         obj.pred_angle = 2;
                     end
                 elseif pred_2 == 0 % 7 (1) - 8 (0)
-                    pred_3 = obj.svmPredict(obj.model.model3_7_8, fr_avg(1,:));
-%                     pred_3 = predict(obj.model.LDA_7_8, fr_avg(1,:));
+%                     pred_3 = obj.svmPredict(obj.model.model3_7_8, fr_avg(1,:));
+                    pred_3 = predict(obj.model.LDA_7_8, fr_avg(1,:));
                     if pred_3 == 1 
                         obj.pred_angle = 7;
                     else
@@ -408,16 +408,16 @@ classdef svmClassifier < handle
             elseif pred_1 == 0 % right(0): 3, 4, 5, 6 
                 pred_2 = obj.svmPredict(obj.model.model2_34_56, fr_avg(1,:)); % 3, 4 (1) - 5, 6 (0)
                 if pred_2 == 1 % 3 (1) - 4 (0)
-                    pred_3 = obj.svmPredict(obj.model.model3_3_4, fr_avg(1,:));
-%                     pred_3 = predict(obj.model.LDA_3_4, fr_avg(1,:));
+%                     pred_3 = obj.svmPredict(obj.model.model3_3_4, fr_avg(1,:));
+                    pred_3 = predict(obj.model.LDA_3_4, fr_avg(1,:));
                     if pred_3 == 1
                         obj.pred_angle = 3;
                     else 
                         obj.pred_angle = 4;
                     end
                 elseif pred_2 == 0 % 5 (1) - 6 (0)
-                    pred_3 = obj.svmPredict(obj.model.model3_5_6, fr_avg(1,:)); 
-%                     pred_3 = predict(obj.model.LDA_5_6, fr_avg(1,:)); 
+%                     pred_3 = obj.svmPredict(obj.model.model3_5_6, fr_avg(1,:)); 
+                    pred_3 = predict(obj.model.LDA_5_6, fr_avg(1,:)); 
                     if pred_3 == 1 
                         obj.pred_angle = 5;
                     else
