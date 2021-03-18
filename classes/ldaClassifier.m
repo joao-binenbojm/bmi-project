6d97ef4a5888440ccbf2df3e8b5daaa0903322ff
Rubén Ruiz-Mateos Serrano <rr1017@ic.ac.uk>
2021-03-18 15:42:25 +0000

    Modified classifier classes to account for new fr_features and PCA.

--- classes/ldaClassifier.m
+++ classes/ldaClassifier.m
@@ -27,7 +27,7 @@ classdef ldaClassifier < handle
             obj.P = V(:,I);
         end
         
-        function [fr_total, fr_avg, X, obj] = fr_features(obj,data,dt,N)
+        function [fr_avg, X, obj] = fr_features(obj,data,N)
             %FR_FEATURES Calculates the firing rate of the data in bins of size dt.
             % data - given data struct
             % dt - time bin size
@@ -39,33 +39,16 @@ classdef ldaClassifier < handle
 
             [T,A] = size(data); %get trial and angle length
 
-            acc = 1;
+            acc = 0;
             fr_avg = zeros(T*A,98); % initialise variables
             fr_avg1 = zeros(T*A,98); % initialise variables
             fr_avg2 = zeros(T*A,98); % initialise variables
-            fr_total = zeros(T*A,N/dt*98);
             for t=1:1:T
                 for a=1:1:A
-                    fr = zeros(98,length(0:dt:N)-1);
-                    fr1 = zeros(98,length(0:dt:N)-1);
-                    fr2 = zeros(98,length(0:dt:N)-1);
-                    for u=1:1:98
-                        var = data(t,a).spikes(u,1:N);
-                        var_alt = var;
-                        var_alt(var_alt==0) = NaN; % make zeros equal to NaN
-                        count = histcounts([1:1:N].*var_alt,0:dt:N); % count spikes in every dt bin until N
-                        fr(u,:) = count/dt;
-                        count1 = sum(var(1:200)); % count spikes in every dt bin until 200
-                        fr1(u,:) = count1/200;
-                        count2 = sum(var(200:320)); % count spikes in every dt bin from  250 to 320
-                        fr2(u,:) = count2/120;
-                    end
-                    fr_avg(acc,:) = mean(fr,2); % get mean firing rate across bins
-                    fr_avg1(acc,:) = mean(fr1,2); % get mean firing rate across bins
-                    fr_avg2(acc,:) = mean(fr2,2); % get mean firing rate across bins
-                    f = reshape(fr,size(fr,1)*size(fr,2),1);
-                    fr_total(acc,:) = f; % get all firing rates ordered in 98 blocks of the same bin
                     acc = acc+1;
+                    fr_avg(acc,:) = mean(data(t,a).spikes(:,1:N),2); % get mean firing rate across bins
+                    fr_avg1(acc,:) = mean(data(t,a).spikes(:,1:200),2); % get mean firing rate across bins
+                    fr_avg2(acc,:) = mean(data(t,a).spikes(:,200:320),2); % get mean firing rate across bins
                 end
             end
             X = [fr_avg,fr_avg1,fr_avg2];
@@ -75,10 +58,8 @@ classdef ldaClassifier < handle
             %FIT(trainingData) Trains model based on training data
             
             [T,~] = size(trainingData); % get size of training data
-    
-            N = 560; % define end time
 
-            [~,~,X] = obj.fr_features(trainingData,80,N); % obtaining firing rate feature space from training data
+            [~,X] = obj.fr_features(trainingData,560); % obtaining firing rate feature space from training data
             obj.fr_norm.mean = mean(X,1);
             obj.fr_norm.std = std(X,1);
             X = (X-obj.fr_norm.mean)./obj.fr_norm.std;
@@ -97,7 +78,7 @@ classdef ldaClassifier < handle
             %test data
             
             N = length(testData.spikes);
-            [~,~,X] = obj.fr_features(testData,80,N); % preprocess EEG data
+            [~,X] = obj.fr_features(testData,N); % preprocess EEG data
             X = (X-obj.fr_norm.mean)./obj.fr_norm.std;
             X(isnan(X)) = 0;
             X(isinf(X)) = 0;
