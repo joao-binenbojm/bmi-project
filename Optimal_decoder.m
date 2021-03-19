00e12e8b8cde1b4d2b824b61676f4bfbffc8c202
Rubén Ruiz-Mateos Serrano <rr1017@ic.ac.uk>
2021-03-19 10:34:23 +0000

    Added decoder comparison data and created optimal decoder script.

---
+++ Optimal_decoder.m
@@ -0,0 +1,44 @@
+%% RANDOM SEED RMSE CALCULATOR
+
+RMSE = zeros(1,50);
+t = zeros(1,50);
+percentage = zeros(1,50);
+f = waitbar(0,'Processing...');
+for itr = 1:50
+    [RMSE(itr),t(itr),modelParameters] = testFunction_for_students_MTb('kalman',true);
+    waitbar(itr/50,f,'Processing...');
+end
+close(f);
+beep;
+
+%% Plots
+
+% Classifier comparison
+% 3d
+
+figure;
+load('Decoder_comparison.mat');
+gm_Mean = gmdistribution([mean(Mean.RMSE),mean(Mean.t)],[std(Mean.RMSE),std(Mean.t)]);
+plt_Mean = fsurf(@(x,y)reshape(pdf(gm_Mean,[x(:),y(:)]),size(x)),[min(Mean.RMSE)-10 max(Mean.RMSE)+10 min(Mean.t)-10 max(Mean.t)+10]);
+plt_Mean.FaceColor = 'b';
+plt_Mean.ShowContours = 'on';
+hold on;
+gm_Kalman = gmdistribution([mean(Kalman.RMSE),mean(Kalman.t)],[std(Kalman.RMSE),std(Kalman.t)]);
+plt_Kalman = fsurf(@(x,y)reshape(pdf(gm_Kalman,[x(:),y(:)]),size(x)),[min(Kalman.RMSE)-10 max(Kalman.RMSE)+10 min(Kalman.t)-10 max(Kalman.t)+10]);
+plt_Kalman.FaceColor = 'g';
+plt_Kalman.ShowContours = 'on';
+xlabel('RMSE','Fontsize',20);
+ylabel('Running time [s]','Fontsize',20);
+legend('Mean trajectory','Kalman');
+
+%Contour
+
+figure;
+gmPDF = @(x,y) arrayfun(@(x0,y0) pdf(gm_Mean,[x0 y0]),x,y);
+fcontour(gmPDF,[min(Mean.RMSE)-10 max(Mean.RMSE)+10 min(Mean.t)-10 max(Mean.t)+10],'MeshDensity',100,'Fill','off','LineWidth',2);
+hold on;
+gmPDF = @(x,y) arrayfun(@(x0,y0) pdf(gm_Kalman,[x0 y0]),x,y);
+fcontour(gmPDF,[min(Kalman.RMSE)-10 max(Kalman.RMSE)+10 min(Kalman.t)-10 max(Kalman.t)+10],'MeshDensity',100,'Fill','off','LineWidth',2);
+set(gca,'Fontsize',15);
+xlabel('RMSE','Fontsize',20);
+ylabel('Running time [s]','Fontsize',20);
