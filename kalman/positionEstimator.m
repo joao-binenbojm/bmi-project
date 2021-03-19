6e062a375deaac9ac5b2d217d23d38f8bb5ae759
Rubén Ruiz-Mateos Serrano <rr1017@ic.ac.uk>
2021-03-19 09:32:17 +0000

    Tested kalman model with optimal LDA classifier. RMSE = 8.8093 for 2013 seed.

--- kalman/positionEstimator.m
+++ kalman/positionEstimator.m
@@ -1,52 +1,47 @@
-function [x, y, model] = positionEstimator(test_data, model)
+function [x, y, model] = positionEstimator(testData, model)
+    % - test_data:
+    %     test_data(m).trialID
+    %         unique trial ID
+    %     test_data(m).startHandPos
+    %         2x1 vector giving the [x y] position of the hand at the start
+    %         of the trial
+    %     test_data(m).decodedHandPos
+    %         [2xN] vector giving the hand position estimated by your
+    %         algorithm during the previous iterations. In this case, N is 
+    %         the number of times your function has been called previously on
+    %         the same data sequence.
+    %     test_data(m).spikes(i,t) (m = trial id, i = neuron id, t = time)
+    %     in this case, t goes from 1 to the current time in steps of 20
+    %     Example:
+    %         Iteration 1 (t = 320):
+    %             test_data.trialID = 1;
+    %             test_data.startHandPos = [0; 0]
+    %             test_data.decodedHandPos = []
+    %             test_data.spikes = 98x320 matrix of spiking activity
+    %         Iteration 2 (t = 340):
+    %             test_data.trialID = 1;
+    %             test_data.startHandPos = [0; 0]
+    %             test_data.decodedHandPos = [2.3; 1.5]
+    %             test_data.spikes = 98x340 matrix of spiking activity
+  
+    N = length(testData.spikes); % get trial length
 
-  % **********************************************************
-  %
-  % You can also use the following function header to keep your state
-  % from the last iteration
-  %
-  % function [x, y, newModelParameters] = positionEstimator(test_data, modelParameters)
-  %                 ^^^^^^^^^^^^^^^^^^
-  % Please note that this is optional. You can still use the old function
-  % declaration without returning new model parameters. 
-  %
-  % *********************************************************
-  
+    % Classification testing
+    C_param = model.C_param; % extract classification parameters
 
-  % - test_data:
-  %     test_data(m).trialID
-  %         unique trial ID
-  %     test_data(m).startHandPos
-  %         2x1 vector giving the [x y] position of the hand at the start
-  %         of the trial
-  %     test_data(m).decodedHandPos
-  %         [2xN] vector giving the hand position estimated by your
-  %         algorithm during the previous iterations. In this case, N is 
-  %         the number of times your function has been called previously on
-  %         the same data sequence.
-  %     test_data(m).spikes(i,t) (m = trial id, i = neuron id, t = time)
-  %     in this case, t goes from 1 to the current time in steps of 20
-  %     Example:
-  %         Iteration 1 (t = 320):
-  %             test_data.trialID = 1;
-  %             test_data.startHandPos = [0; 0]
-  %             test_data.decodedHandPos = []
-  %             test_data.spikes = 98x320 matrix of spiking activity
-  %         Iteration 2 (t = 340):
-  %             test_data.trialID = 1;
-  %             test_data.startHandPos = [0; 0]
-  %             test_data.decodedHandPos = [2.3; 1.5]
-  %             test_data.spikes = 98x340 matrix of spiking activity
-  
-  
-  
-  % ... compute position at the given timestep.
-  
-  % Return Value:
-  % - [x, y]:
-  %     current position of the hand
-  
-  [x, y, model] = model.predict(test_data, test_data.angle);
+    if N==320
+        pred_angle = C_param.LDA1.predict(testData); % classify angle from LDA 
+    elseif N==440
+        pred_angle = C_param.LDA2.predict(testData);
+    elseif N==560
+        pred_angle = C_param.LDA3.predict(testData);
+    else
+        pred_angle = model.pred_angle;
+    end 
+
+    model.pred_angle = pred_angle;
+    
+    [x, y, model.R_param] = model.R_param.predict(testData, pred_angle);
    
 end
 
--- kalman/positionEstimatorTraining.m
+++ kalman/positionEstimatorTraining.m
@@ -1,18 +1,24 @@
-function [model] = positionEstimatorTraining(training_data)
-  % Arguments:
+function [model] = positionEstimatorTraining(trainingData)
+    % Arguments:
+
+    % - training_data:
+    %     training_data(n,k)              (n = trial id,  k = reaching angle)
+    %     training_data(n,k).trialId      unique number of the trial
+    %     training_data(n,k).spikes(i,t)  (i = neuron id, t = time)
+    %     training_data(n,k).handPos(d,t) (d = dimension [1-3], t = time)
   
-  % - training_data:
-  %     training_data(n,k)              (n = trial id,  k = reaching angle)
-  %     training_data(n,k).trialId      unique number of the trial
-  %     training_data(n,k).spikes(i,t)  (i = neuron id, t = time)
-  %     training_data(n,k).handPos(d,t) (d = dimension [1-3], t = time)
-  model = KalmanModel();
-  model = model.fit(training_data);
-  
-  % Return Value:
-  
-  % - model object:
-  %     single model object that represents the trained model with
-  %     appropriate model parameters
+    C_param = struct;
+
+    % Classification training
+    C_param.LDA1 =  ldaClassifier(1);
+    model.C_param.LDA1 = C_param.LDA1.fit(trainingData);
+    C_param.LDA2 = ldaClassifier(2);
+    model.C_param.LDA2 = C_param.LDA2.fit(trainingData);
+    C_param.LDA3 = ldaClassifier(3);
+    model.C_param.LDA3 = C_param.LDA3.fit(trainingData);
+    
+    R_param = KalmanModel();
+    model.R_param = R_param.fit(trainingData);
+    model.pred_angle = [];
   
 end
