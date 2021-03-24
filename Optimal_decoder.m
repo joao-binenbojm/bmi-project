5f82db71b5e4c903cf3d2eb6ac2ab4e2f17dd4a4
Rubén Ruiz-Mateos Serrano <rr1017@ic.ac.uk>
2021-03-24 08:49:23 +0000

    Adjusted to changes.

--- Optimal_decoder.m
+++ Optimal_decoder.m
@@ -5,7 +5,7 @@ t = zeros(1,100);
 percentage = zeros(1,100);
 f = waitbar(0,'Processing...');
 for itr = 1:100
-    [RMSE(itr),t(itr),modelParameters] = testFunction_for_students_MTb('kalman',true);
+    [RMSE(itr),t(itr),modelParameters] = testFunction_for_students_MTb('kalman',true,false);
     waitbar(itr/100,f,'Processing...');
 end
 close(f);
